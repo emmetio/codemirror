5d5ab23a62e3b91e10c416db790b9421679cb43b
Sergey Chikuyonok <serge.che@gmail.com>
2014-04-14 12:49:10 +0400

    Added Gulp builder

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var gulp = require('gulp');
+var requirejs = require('gulp-requirejs');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+gulp.task('default', function() {
+	// load JSON data
+	var snippets = fs.readFileSync('./node_modules/emmet/lib/snippets.json', {encoding: 'utf8'});
+	var caniuse = fs.readFileSync('./node_modules/emmet/lib/caniuse.json', {encoding: 'utf8'});
+
+	var loadData = 'var emmet = require("emmet/emmet");emmet.loadUserData({' +
+		'snippets: ' + snippets + ',' +
+		'caniuse: ' + caniuse +
+		'});';
+
+	return requirejs({
+		baseUrl: './',
+		name: 'vendor/almond',
+		include: ['./plugin'],
+		out: 'emmet.js',
+		paths: {
+			emmet: 'node_modules/emmet/lib',
+			lodash: 'node_modules/emmet/node_modules/lodash/lodash'
+		},
+		wrap: {
+			start: '(function (root, factory) {if (typeof define === "function" && define.amd) {define(factory);} else {root.emmetPlugin = factory();}}(this, function () {',
+			end: ';' + loadData + 'return require(\'plugin\');}));'
+		}
+	})
+	.pipe(gulp.dest('./dist'))
+	.pipe(rename('emmet.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('./dist'));
+});
